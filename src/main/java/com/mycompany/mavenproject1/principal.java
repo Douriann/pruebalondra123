71b5be126b12303301e0e59143e8fa988428a649
Alondra-Leon <alondraleonorsini46@gmail.com>
2024-06-27 23:31:58 -0400

    enviando segunda version

--- src/main/java/com/mycompany/mavenproject1/principal.java
+++ src/main/java/com/mycompany/mavenproject1/principal.java
@@ -3,6 +3,11 @@ package com.mycompany.mavenproject1;
 
 public class principal {
     public static void main(String[] args){
-        System.out.println("hola");
+        System.out.println("hola, como estas?");
+        int num1, num2;
+        num1 = 2;
+        num2 = 6;
+        int suma = num1 + num2;
+        System.out.println("La suma es " + suma);
     }
 }
