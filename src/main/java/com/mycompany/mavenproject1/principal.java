68e0dfd952185d4507865c9ea42d5dad18677c03
Douriann <scoclox@gmail.com>
2024-06-28 00:01:16 -0400

    ultima version

--- src/main/java/com/mycompany/mavenproject1/principal.java
+++ src/main/java/com/mycompany/mavenproject1/principal.java
@@ -1,8 +1,12 @@
 
 package com.mycompany.mavenproject1;
+import interfazpaquete.Interfaz;
 
 public class principal {
     public static void main(String[] args){
         System.out.println("Bienvenido al sistema");
+        Interfaz ventana = new Interfaz();
+        ventana.setVisible(true);
+        ventana.setLocationRelativeTo(null);
     }
 }
